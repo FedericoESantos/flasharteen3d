93ab3e5c50d44361be75f9a5af61efd495e9232b
Federico <boomarts@yahoo.com>
2025-11-10 19:43:51 -0300

    por terminar proyecto de app en react e commerce empresa impresiones 3D

--- src/components/InfoMain/InfoMain.jsx
+++ src/components/InfoMain/InfoMain.jsx
@@ -4,7 +4,8 @@ const InfoMain = () => {
   return (
     <div className='infomain'>
       <span>
-        Realizamos trabajos de impresión 3D de todo tipo: desde Juegos y juguetes, muñecos y máscaras, hasta herramientas funcionales. También hacemos trabajos personalizados a medida <strong>(consultar). </strong> 
+        Realizamos trabajos de impresión 3D de todo tipo: desde Juegos y juguetes, muñecos y máscaras, hasta herramientas funcionales. <br />
+        <h2>También hacemos trabajos personalizados a medida <strong>(consultar). </strong> </h2>
         Además, podemos digitalizar una foto y convertirla en un busto decorativo.
       </span>
     </div>
